6b4008969f54f05affc8c053fa260480b6a54218
ethansudman@comcast.net <ethansudman@comcast.net>
2021-06-20 16:29:29 -0500

    Fix logging and call

--- Research/MultiprocessGetData.py
+++ Research/MultiprocessGetData.py
@@ -32,7 +32,7 @@ def doCalculations(file):
     
         G = nx.read_graphml(file)
         
-        f.write('opened graph')
+        f.write('Opened graph\n')
         data['Degree Assortativity Coefficient'] = nx.degree_assortativity_coefficient(G)
         data['Nodes'] = nx.number_of_nodes(G)
         data['Edges'] = nx.number_of_edges(G)
@@ -43,23 +43,24 @@ def doCalculations(file):
         data['Average Degree'] = sum(dict(G.degree()).values()) / nx.number_of_nodes(G)
         data['Connected Components'] = sum(1 for component in nx.connected_components(G))
         
-        f.write('About to start community processing')
+        f.write('About to start community processing\n')
         data['Girvan-Newman Method Communities'] = sum(1 for community in nx.algorithms.community.girvan_newman(G))
         #df = pd.DataFrame(data, columns = ['Colony', 'Day', 'Degree Assortativity Coefficient', 'Average Clustering', 'Nodes', 'Edges', 'Density', 'Diameter', 'Betweenness Centrality', 'Connected Components', 'Girvan-Newman Method Communities', 'Local Efficiency', 'Global Efficiency', 'Small-World Coefficient (Sigma)', 'Small-World Coefficient (Omega)'])
         
-        f.write('About to start data frame')
+        f.write('About to start data frame\n')
         df = pd.DataFrame(data)
-        f.write('About to do to_csv')
+        f.write('About to do to_csv\n')
         df.to_csv('Graph Descriptive Statistics - Colony ' + m[0] + ' Day ' + m[1] + '.csv')
         f.write('Did to_csv')
     
-processes = []
-
-for file in glob.glob("*.graphml"):
-    p = Process(target = doCalculations, args = (file))
-    p.start()
+if __name__ == '__main__':
+    processes = []
     
-    processes.append(p)
-    
-for p in processes:
-    p.join()
+    for file in glob.glob("*.graphml"):
+        p = Process(target = doCalculations, args = (file,))
+        p.start()
+        
+        processes.append(p)
+        
+    for p in processes:
+        p.join()
